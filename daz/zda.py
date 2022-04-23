2592f578bd0e34ba7823de03162d751fe8925a91
CATS <11648807+CoAuToSe@users.noreply.github.com>
2022-04-23 10:59:34 +0200

    init

---
+++ daz/zda.py
@@ -0,0 +1,9 @@
+import mypythonlib
+from mypythonlib import myfunctions
+
+print(myfunctions.haversine(52.370216, 4.895168, 52.520008,
+    13.404954) )
+
+import Themeparks
+from Themeparks import some
+print(some.I_work())
