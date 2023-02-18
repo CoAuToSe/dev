1d617d4d59f3761fc687e78005742efbabac03b0
AER <Aurelien.Couthures@gmail.com>
2023-02-18 16:55:07 +0100

    web_sys_stuff
    unify is the most uification possible of all those examples

---
+++ web_sys_stuff/unify/src/console.rs
@@ -0,0 +1,37 @@
+use crate::js_fn::*;
+use wasm_bindgen::prelude::*;
+
+pub fn bare_bones() {
+    log("Hello from Rust!");
+    log_u32(42);
+    log_many("Logging", "many values!");
+}
+
+// Next let's define a macro that's like `println!`, only it works for
+// `console.log`. Note that `println!` doesn't actually work on the wasm target
+// because the standard library currently just eats all output. To get
+// `println!`-like behavior in your app you'll likely want a macro like this.
+
+macro_rules! console_log {
+    // Note that this is using the `log` function imported above during
+    // `bare_bones`
+    ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
+}
+
+pub fn using_a_macro() {
+    console_log!("Hello {}!", "world");
+    console_log!("Let's print some numbers...");
+    console_log!("1 + 3 = {}", 1 + 3);
+}
+
+// And finally, we don't even have to define the `log` function ourselves! The
+// `web_sys` crate already has it defined for us.
+
+pub fn using_web_sys() {
+    use web_sys::console;
+
+    console::log_1(&"Hello using web-sys".into());
+
+    let js: JsValue = 4.into();
+    console::log_2(&"Logging arbitrary values looks like".into(), &js);
+}
