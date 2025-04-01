8667778504e94dcf8be6d8ba0748ceae94fb2fee
CATS <11648807+CoAuToSe@users.noreply.github.com>
2025-04-01 16:11:04 +0200

    Update build.rs

--- hello_torch_rust/build.rs
+++ hello_torch_rust/build.rs
@@ -1,10 +1,11 @@
-const RELATIV_PATH: &str = "..\\libtorch\\libtorch";
+// TODO: change `.cargo/config.toml` too 
+const RELATIV_PATH: &str = "..\\libtorch\\libtorch\"";
 
 fn main() {
     let dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
 
     // $Env:LIBTORCH = "C:\\Users\\Aurel\\OneDrive\\Documents\\dev2\\torch\\libtorch\\libtorch"
-    println!("cargo:rustc-env=LIBTORCH=\"{dir}\\{RELATIV_PATH}\"");
+    println!("cargo:rustc-env=LIBTORCH=\"{dir}\\{RELATIV_PATH}");
 
     let path = std::env::var("Path").unwrap();
     // $Env:Path += ";C:\\Users\\Aurel\\OneDrive\\Documents\\dev2\\torch\\libtorch\\libtorch\\lib"
